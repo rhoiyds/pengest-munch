07468094b1c62e1562fcc86ea29476f4907de778
Roy Porter <royporter7@gmail.com>
2017-02-15 19:10:28 +1100

    inital commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp        = require('gulp'),
+    browserSync = require('browser-sync');
+
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+
+
+gulp.task('default', ['browser-sync']);
