3ac7eaddd3b2397d161943bf037c79991783f311
Roy Porter <royporter7@gmail.com>
2017-02-20 18:46:50 +1100

    Added all the data

--- js/peng.controller.js
+++ js/peng.controller.js
@@ -42,6 +42,114 @@ angular.module('pengApp').controller('pengestController', ['$scope', '$http',
               wings: 5,
               total: 4
             }
+          }, {
+            name: "Taste of Tennessee",
+            location: {
+              latitude: 51.527163,
+              longitude: -0.083739
+            },
+            rating: {
+              burger: 3,
+              chips: 1.5,
+              wings: 4.2,
+              total: 2.9
+            }
+          }, {
+            name: "Dixy",
+            location: {
+              latitude: 51.5561328,
+              longitude: -0.1390873
+            },
+            rating: {
+              burger: 3,
+              chips: 2,
+              wings: 2,
+              total: 2
+            }
+          } , {
+            name: "Dallas",
+            location: {
+              latitude: 51.5036952,
+              longitude: -0.1150325
+            },
+            rating: {
+              burger: 4.6,
+              chips: 4,
+              wings: 4.3,
+              total: 4.5
+            }
+          } , {
+            name: "Chicken Valley",
+            location: {
+              latitude: 51.52099119,
+              longitude: -0.1712288
+            },
+            rating: {
+              burger: 1.2,
+              chips: 2.9,
+              wings: 1.9,
+              total: 1.5
+            }
+          } , {
+            name: "Chick King",
+            location: {
+              latitude: 51.6044748,
+              longitude: -0.0682239
+            },
+            rating: {
+              burger: 1.2,
+              chips: 3,
+              wings: 3.5,
+              total: 3.5
+            }
+          }, {
+            name: "Sams Chicken",
+            location: {
+              latitude: 51.613932,
+              longitude: -0.1769936
+            },
+            rating: {
+              burger: 3.8,
+              chips: 3,
+              wings: 4,
+              total: 4.3
+            }
+          } , {
+            name: "Morleys",
+            location: {
+              latitude: 51.398632,
+              longitude: -0.096530
+            },
+            rating: {
+              burger: 4.2,
+              chips: 4.1,
+              wings: 4.5,
+              total: 4.3
+            }
+          }, {
+            name: "Chicken Valley",
+            location: {
+              latitude: 51.5058768,
+              longitude: -0.2278134
+            },
+            rating: {
+              burger: 1.7,
+              chips: 3.2,
+              wings: 2.3,
+              total: 1.7
+            }
+          }, {
+            name: "KRFC",
+            location: {
+              latitude: 51.5058768,
+              longitude: -0.032140
+            },
+            rating: {
+              burger: 3.3,
+              chips: 3.2,
+              wings: 4.6,
+              total: 4
+            }
           }
         ]
 
