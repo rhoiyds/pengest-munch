8c705e4d56c01e21994a237413eedec0e30fb7c7
Roy Porter <roy@lotj.com>
2017-02-21 09:45:29 +1100

    Charts properly load review data

--- js/peng.controller.js
+++ js/peng.controller.js
@@ -2,10 +2,7 @@ angular.module('pengApp', ["chart.js"]).controller('pengestController', ['$scope
 
     function($scope, $http) {
 
-        //Variable to indicate whether to show the final summary page.
-        $scope.text = "Hello world";
-
-        $scope.reviews = [
+      $scope.reviews = [
           {
             name: "Chicken Run",
             location: {
@@ -195,7 +192,13 @@ angular.module('pengApp', ["chart.js"]).controller('pengestController', ['$scope
                 $scope.map.panTo(marker.getPosition());
                 infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
                 infoWindow.open($scope.map, marker);
-                $scope.showChart()
+                review = null;
+                for (i = 0; i < $scope.reviews.length; i++){
+                  if ($scope.reviews[i].marker == marker) {
+                    review = $scope.reviews[i];
+                  }
+                }
+                $scope.showChart(review);
             });
 
             review.marker = marker;
@@ -206,7 +209,7 @@ angular.module('pengApp', ["chart.js"]).controller('pengestController', ['$scope
           $scope.map.panTo(review.marker.getPosition());
           infoWindow.setContent('<h2>' + review.marker.title + '</h2>' + review.marker.content);
           infoWindow.open($scope.map, review.marker);
-          $scope.showChart();
+          $scope.showChart(review);
         }
 
         for (i = 0; i < $scope.reviews.length; i++){
@@ -217,30 +220,23 @@ angular.module('pengApp', ["chart.js"]).controller('pengestController', ['$scope
             e.preventDefault();
           }
 
-          $scope.showChart = function() {
+          $scope.showChart = function(review) {
             var ctx = document.getElementById("myChart");
             var myChart = new Chart(ctx, {
                 type: 'bar',
                 data: {
-                    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                    labels: ["Chips", "Wings", "Burger"],
                     datasets: [{
-                        label: '# of Votes',
-                        data: [12, 19, 3, 5, 2, 3],
+                        data: [review.rating.chips, review.rating.wings, review.rating.burger],
                         backgroundColor: [
                             'rgba(255, 99, 132, 0.2)',
                             'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
+                            'rgba(255, 206, 86, 0.2)'
                         ],
                         borderColor: [
                             'rgba(255,99,132,1)',
                             'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
+                            'rgba(255, 206, 86, 1)'
                         ],
                         borderWidth: 1
                     }]
@@ -249,7 +245,9 @@ angular.module('pengApp', ["chart.js"]).controller('pengestController', ['$scope
                     scales: {
                         yAxes: [{
                             ticks: {
-                                beginAtZero:true
+                                beginAtZero:true,
+                                max: 5,
+                                stepValue: 1
                             }
                         }]
                     }
